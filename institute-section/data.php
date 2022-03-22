a85540097a0f3e4ccc1e12925c7b869bd4399647
DhanashriMirajkar <dhanashri.mirajkar20@gmail.com>
2022-03-22 09:29:29 +0530

    All Files

---
+++ institute-section/data.php
@@ -0,0 +1,9 @@
+<?php
+   // mysqli connection parameters
+   $hostname = "localhost";
+   $username = "root"; 
+   $password = "";
+   $dbname = "exam";
+   $college = "DKTE's Yashwantrao Chavan Polytechnic, Ichalkaranji";
+   $host = "http://localhost/VBD";
+?>
